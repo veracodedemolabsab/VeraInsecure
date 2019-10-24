03a03edbdf13690c07ffdae730c5ede752ae66df
m-browntown <sdmattbrown@gmail.com>
2019-10-24 13:46:42 -0700

    Change flaws and add srcclr

--- src/main/java/xyz/veracode/verainsecure/OpenRedirect.java
+++ src/main/java/xyz/veracode/verainsecure/OpenRedirect.java
@@ -1,6 +1,7 @@
 package xyz.veracode.verainsecure;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -30,19 +31,18 @@ public class OpenRedirect extends HttpServlet {
 		String query = request.getQueryString();
 		
 		//CWE-113: Improper Neutralization of CRLF Sequences in HTTP Headers ('HTTP Response Splitting')
-		//
 		//CWE 601: Open Redirects
-		//For example http://localhost:8085/exampleflaws/openredirect?unvalidatedurl=http://www.google.com
-		if (query.contains("unvalidatedurl")) {
-			String url = request.getParameter("unvalidatedurl");
+		//For example http://localhost:7001/exampleflaws/openredirect?badurl=http%3A%2F%2Fwww.google.com%0A
+		if (query.contains("badurl")) {
+			String url = request.getParameter("badurl");
 			response.sendRedirect(url);
 		}
 		
 		//Stop CWE 113 and create Best Practice with Veracode Approved Library
 		//Stop CWE 601 with Veracode Approved Library
-		if (query.contains("validatedurl")) {
-			String validatedurl = request.getParameter("validatedurl");
-			response.sendRedirect(URLEncoder.encode(validatedurl, "UTF-8"));
+		if (query.contains("goodurl")) {
+			String url = request.getParameter("goodurl");
+			response.sendRedirect(URLEncoder.encode(url, "UTF-8"));
 		}
 	}
 }
